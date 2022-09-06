9d1ce6cd0d70d55b378d463c9771f472f89a9b93
Maksim Eremin <eremin1988ma21@gmail.com>
2022-09-06 20:20:02 +0300

    update package

--- repository.js
+++ repository.js
@@ -1,17 +1,13 @@
 const mongoose = require('mongoose');
 // 4jsj4Q28TV8TQd1U
 const main = async () => {
-    await mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://eremin-server-aws:4jsj4Q28TV8TQd1U@node-server-cluster.xdfgqyq.mongodb.net/TEST?retryWrites=true&w=majority',
-        {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useUnifiedTopology: true
-        });
+    await mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://eremin-server-aws:4jsj4Q28TV8TQd1U@node-server-cluster.xdfgqyq.mongodb.net/TEST?retryWrites=true&w=majority');
+    console.log("Mongo connected")
 }
 main().catch(err => console.log(err));
 
-// const usersSchema = new mongoose.Schema({name: String});
-// const MyUsers = mongoose.model('MyUsers', usersSchema);
+const usersSchema = new mongoose.Schema({name: String});
+const MyUsers = mongoose.model('MyUsers', usersSchema);
 
 const getUsers = (search) => {
     if (!search) {
@@ -46,4 +42,5 @@ module.exports.addUser = addUser;
 module.exports.deleteUser = deleteUser;
 module.exports.getUser = getUser;
 module.exports.updateUser = updateUser;
+// module.exports.main = main;
 
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 // const baseURL = 'https://polar-beach-73696.herokuapp.com/' //4jsj4Q28TV8TQd1U
+const mongoose = require('mongoose');
+
 const express = require('express')
 const app = express()
 
@@ -25,7 +27,14 @@ app.use((req, res) => {
     res.send(404);
 });
 
-app.listen(port, () => {
+// const main = async () => {
+//     await mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://eremin-server-aws:4jsj4Q28TV8TQd1U@node-server-cluster.xdfgqyq.mongodb.net/TEST?retryWrites=true&w=majority');
+// }
+
+
+app.listen(port, async () => {
+    // await main().catch(err => console.log(err));
+
     console.log(`Server iss running on ${port}`)
 })
 
